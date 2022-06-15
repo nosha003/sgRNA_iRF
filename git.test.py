7be3c84b06e17acc762311ef4254aaad9500648a
Jaclyn Noshay <jmnosh@gmail.com>
2022-06-15 16:21:46 -0400

    updated

--- git.test.py
+++ git.test.py
@@ -1,3 +1,5 @@
 print ("6/7/22 bioRxiv submission")
 print ("6/9/22 Nature Biotechnology submission")
 print ("6/14/22 Nature Biotechnology editorial rejection")
+
+print ("6/15/22 Consider next submission journal... Nature Communications? Genome Research? Nucleic Acids Research?")
