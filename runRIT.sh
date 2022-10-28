443eefbab48b57d86654aee6047cd7ad0816e7b2
nosha003 <32301282+nosha003@users.noreply.github.com>
2022-10-28 10:32:10 -0400

    Create runRIT.sh

---
+++ runRIT.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Code written by Jonathon Romero
+
+#Usage: runRIT.sh feature name
+
+#name of feature
+feature=$1
+#echo $feature
+
+#pre-name
+prename=$2
+#echo $name
+
+threaded=$3
+
+module load python/3.7-anaconda3
+
+cat ${prename}*.pathfile > ${prename}_${feature}.paths
+python /gpfs/alpine/syb105/proj-shared/Personal/jromero/PathAnalysis/trimpath.py ${prename}_${feature}.paths
+mv ${prename}_${feature}.paths ${prename}_${feature}.paths.old
+mv ${prename}_${feature}.paths.fixed ${prename}_${feature}.paths
+python /gpfs/alpine/syb105/proj-shared/Personal/jromero/PathAnalysis/preprocessPathsForRIT.py ${prename}_${feature}.paths
+/gpfs/alpine/syb105/proj-shared/Personal/jromero/codesnippets/ritw ${prename}_${feature}.paths.int.out > ${prename}_${feature}.full
+_rit
+sort -k1rg ${prename}_${feature}.full_rit > ${prename}_${feature}.full_rit_sort
+head -n 20 ${prename}_${feature}.full_rit_sort > ${prename}_${feature}.rit 
+python /gpfs/alpine/syb105/proj-shared/Personal/jromero/PathAnalysis/ritEval.py ${prename}_${feature}.importance4 ${feature} $threa
+ded 
+sort -k3rg ${prename}_${feature}.importance4.effect > ${prename}.importance4.effect_sorted 
