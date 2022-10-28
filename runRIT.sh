e4a1199839c2ccc41b691305b35b80d9048166da
nosha003 <32301282+nosha003@users.noreply.github.com>
2022-10-28 10:40:42 -0400

    Update runRIT.sh

--- runRIT.sh
+++ runRIT.sh
@@ -18,14 +18,14 @@ module load python/3.7-anaconda3
 # cd into iRF yvec directory
 
 cat ${prename}*.pathfile > ${prename}_${feature}.paths
-python RIT.code/trimpath.py ${prename}_${feature}.paths
+python trimpath.py ${prename}_${feature}.paths
 mv ${prename}_${feature}.paths ${prename}_${feature}.paths.old
 mv ${prename}_${feature}.paths.fixed ${prename}_${feature}.paths
-python RIT.code/preprocessPathsForRIT.py ${prename}_${feature}.paths
-RIT.code/ritw ${prename}_${feature}.paths.int.out > ${prename}_${feature}.full
+python preprocessPathsForRIT.py ${prename}_${feature}.paths
+ritw ${prename}_${feature}.paths.int.out > ${prename}_${feature}.full
 _rit
 sort -k1rg ${prename}_${feature}.full_rit > ${prename}_${feature}.full_rit_sort
 head -n 20 ${prename}_${feature}.full_rit_sort > ${prename}_${feature}.rit 
-python RIT.code/ritEval.py ${prename}_${feature}.importance4 ${feature} $threa
+python ritEval.py ${prename}_${feature}.importance4 ${feature} $threa
 ded 
 sort -k3rg ${prename}_${feature}.importance4.effect > ${prename}.importance4.effect_sorted 
